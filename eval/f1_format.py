f2fffa4c106bbbfa22ca5def47a094f0f59388f0
xkliu <chthollylxk@mail.ustc.edu.cn>
2023-12-18 17:27:21 +0800

    add f1 eval

---
+++ eval/f1_format.py
@@ -0,0 +1,81 @@
+import json
+from tqdm import tqdm
+import re
+
+def result_decode(llm_pred:str, map_dict):
+    llm_pred = llm_pred.lstrip('<|assistant|>\n')
+    llm_pred = llm_pred.lower()
+    '''llm result decode
+        1. decode json like {'entity_name'}
+        2. if fail, use the last entity name in sentence
+    '''
+    decode_flag = False
+    pred_entity = None
+    json_str = re.search(r'{.*}', llm_pred)
+    if json_str:
+        try:
+            json_str = json.loads(json_str.group())
+            if 'name of the entity' in json_str.keys():
+                pred_entity = json_str['name of the entity']
+            else:
+                for v in json_str.values():
+                    if not str(v).isdigit():
+                        pred_entity = v
+            if pred_entity != None:
+                decode_flag = True
+        except:
+            pass
+    if decode_flag:
+        '''check name valid'''
+        try:
+            if pred_entity in map_dict.keys():
+                return pred_entity
+            else:
+                decode_flag = False
+        except:
+            decode_flag = False
+
+    if not decode_flag:
+        index_dict = {}
+        for name in map_dict.keys():
+            if name in llm_pred:
+                pos = llm_pred.rfind(name) + len(name)
+                index_dict[name] = pos
+    
+        if len(index_dict) > 0:
+            pred_entity, _ = sorted(index_dict.items(), key = lambda kv:(kv[1], len(kv[0]), kv[0]), reverse=True)[0]
+        else:
+            pred_entity = 'none'
+    
+    return pred_entity
+
+def file_f1(file_name):
+    all_num = 0
+    hit_num = 0
+    error_num = 0
+    with open(file_name) as input_f:
+        for line in input_f:
+            all_num += 1
+            line = json.loads(line)
+
+            if len(line['llama_predict']) == 0:
+                error_num += 1
+                continue
+
+            map_dict = {'none':-1}
+            for cand in line['candidates']:
+                map_dict[cand['name'].lower()] = int(cand['wiki_id'])
+
+            pred_entity = result_decode(line['llama_predict'], map_dict)
+            pred_num = map_dict[pred_entity]
+
+            if pred_num == line['ans_id']:
+                hit_num += 1
+
+    print(all_num, hit_num, hit_num / all_num ,error_num)
+
+if __name__ == "__main__":
+    datasets_path = '/data/xkliu/EL_datasets/result/'
+    dateset_name = 'wiki_test_prompt0'
+    recall_file = '{}_sum13B_13B_noprompt.jsonl'.format(dateset_name)
+    file_f1(datasets_path + recall_file)
